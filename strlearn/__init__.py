8b9a73aba628bb9056f7bb8feed697eef39a78d4
Wakir <50971678+Wakir@users.noreply.github.com>
2024-01-21 11:50:58 +0100

    Initial commit with Stream-learn

---
+++ strlearn/__init__.py
@@ -0,0 +1,13 @@
+from . import classifiers, ensembles, evaluators, metrics, streams, detectors, utils
+from ._version import __version__
+
+__all__ = [
+    "classifiers",
+    "ensembles",
+    "evaluators",
+    "metrics",
+    "streams",
+    "detectors",
+    "utils",
+    "__version__",
+]
